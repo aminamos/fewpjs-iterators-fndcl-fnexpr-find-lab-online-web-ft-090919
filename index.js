0f4aa501c6f52caf4a72dafda8bf01b3388a1489
aminamos <26092352+aminamos@users.noreply.github.com>
2019-12-17 15:28:41 -0800

    draft

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(a) {
+  a.forEach(function(r) {
+    let keys = Object.keys(r);
+    let elementValues = Object.values(r);
+    
+    if (elementValues.find(function(s) {
+        return s == "W"
+    })) {
+    return console.log(r)
+    }
+})
+}
